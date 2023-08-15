dd63c0251d20bf3dda57e2d9071c7be372368339
Thivakaran <85859982+Thivakaran-R@users.noreply.github.com>
2023-08-15 23:15:49 +0530

    Add files via upload

---
+++ python base code/casting.py
@@ -0,0 +1,30 @@
+"""
+Casting : converting into one datatype into another datatype
+"""
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(type(z))
+
+"""
+sample output:
+s1
+2
+<class 'str'>
+"""
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
+
+"""
+sample output:
+s1
+2
+3.0
+"""
+
