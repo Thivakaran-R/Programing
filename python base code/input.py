954e46477dc9db6d8fa31233208789fa30e912d0
Thivakaran <85859982+Thivakaran-R@users.noreply.github.com>
2023-08-15 23:00:45 +0530

    Add files via upload

---
+++ python base code/input.py
@@ -0,0 +1,44 @@
+
+a = input()
+b = int (input())
+
+print("My Name is:",a)
+print("My age:", b)
+
+
+name = input()
+age = int(input())
+address = input()
+
+print("My Name is:", name)
+print("My age:", age)
+print("My Address:",address)
+
+a= int(input())
+b = int(input())
+c = int(input())
+add = a + b + c
+multi = a * b * c
+
+print(add)
+print(multi)
+div = multi / add
+print(div)
+
+
+name = input()
+score = int(input())
+department = input()
+
+print(name)
+print(score)
+print(department)
+
+
+name = input()
+score = int(input())
+department = input()
+
+print("my name is :",name)
+print("My score is:",score/10,"/10")
+print("my department is :",department)
